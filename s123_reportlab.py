cc3a786ea191e78756bf2a935a639e9bed624a5c
Dave Crawford <38766170+DanCranford@users.noreply.github.com>
2019-08-05 07:49:03 -0700

    switched to platypus

--- s123_reportlab.py
+++ s123_reportlab.py
@@ -19,7 +19,7 @@ class s123_package():
 #        self.alias_attributes = [[self.fm_main[field],self.attributes['field']] for field in list(fm_main.keys())]
         
         if layer.properties.hasAttachments:
-            self.res = layer.attachments.search(sql_query)
+            self.att_res = layer.attachments.search(sql_query)
             
             
     def build_field_order(self):
@@ -27,58 +27,40 @@ class s123_package():
         for field in list(self.fm_main.keys()):
             att_alias.append([self.fm_main[field],self.attributes[field]])
         return att_alias
+    
+    def grab_att_links(self):
+        return [info['DOWNLOAD_URL'] for info in self.att_res]
 
 
-def canvas_it_up(att_data, pdf_path):
-    from reportlab.pdfgen import canvas
-    from reportlab.lib.pagesizes import letter
-    
-    canvas = canvas.Canvas(pdf_path, pagesize=letter)
-    canvas.setLineWidth(.3)
-    canvas.setFont('Helvetica', 12)
-    
-    x = 30
-    y = 750
-    
-    for pair in att_data:
-        canvas.drawString(x, y, str(pair[0]))
-        canvas.drawString(x+100, y, str(pair[1]))
-        y-=15
-    
-    canvas.save()
+#def canvas_it_up(att_data, pdf_path):
+#    from reportlab.pdfgen import canvas
+#    from reportlab.lib.pagesizes import letter
+#    
+#    canvas = canvas.Canvas(pdf_path, pagesize=letter)
+#    canvas.setLineWidth(.3)
+#    canvas.setFont('Helvetica', 12)
+#    
+#    x = 30
+#    y = 750
+#    
+#    for pair in att_data:
+#        canvas.drawString(x, y, str(pair[0]))
+#        canvas.drawString(x+100, y, str(pair[1]))
+#        y-=15
+#    
+#    canvas.save()
         
-
-
-import arcgis
-
-org = 'https://wevm.maps.arcgis.com'
-username = 'WEVMAPS'
-password = 'ckd3sons'
-
-GIS = arcgis.GIS(org,username,password)
-
-item_survey = arcgis.gis.Item(GIS,'cf9e8f3bf6414a08944d0fa29ae736b6')
-
-lyr_main = item_survey.layers[0]
-
-sql_query = "OBJECTID = 5"
-
-mypack = s123_package(lyr_main,sql_query)
-
-
-
-testfile = r"C:\Users\dcraw\Desktop\test1.pdf"
-
-
-import reportlab.platypus as platypus
-from reportlab.lib.styles import getSampleStyleSheet
-
-doc = platypus.SimpleDocTemplate(testfile)
-style_sheet = getSampleStyleSheet()
-p1 = platypus.Paragraph('''Check This shit out /n I think there needs to be html in here''', style_sheet['BodyText'])
-table = platypus.Table(mypack.build_field_order())
-image = platypus.Image(mypack.res[0]['DOWNLOAD_URL'])
-doc.build(p1,table,image)
-
+def generic_feature_report(feature_object, output_file):
+    import reportlab.platypus as platypus
+    from reportlab.lib.styles import getSampleStyleSheet
+    
+    elements = []
+    doc = platypus.SimpleDocTemplate(output_file)
+    style_sheet = getSampleStyleSheet()
+    
+    
+    table = platypus.Table(feature_object.build_field_order())
+    
+    doc.build(elements)
 
 
