3ef1700f112d65e1d5abec517adefe50c28e7ff4
dancranford <dcrawford1@gmail.com>
2019-10-03 09:46:10 -0700

    removed old code

--- s123_reportlab.py
+++ s123_reportlab.py
@@ -5,33 +5,6 @@ Created on Thu Aug  1 14:00:55 2019
 @author: dcraw
 """
 
-#class feature_package(object):
-#    def __init__(self, layer, sql_query):
-#        self.main_fset = layer.query(sql_query)
-#        self.fields_main = self.main_fset.fields
-#        self.fm_main = {}
-#        for field in self.fields_main:
-#            self.fm_main[field['name']]=field['alias']
-#        
-#        self.attributes = self.main_fset.features[0].attributes
-#        self.geometry = self.main_fset.features[0].geometry
-#        
-##        self.alias_attributes = [[self.fm_main[field],self.attributes['field']] for field in list(fm_main.keys())]
-#        
-#        if layer.properties.hasAttachments:
-#            self.att_res = layer.attachments.search(sql_query)
-#            
-#            
-#    def build_field_order(self):
-#        att_alias = []
-#        for field in list(self.fm_main.keys()):
-#            att_alias.append([self.fm_main[field],self.attributes[field]])
-#        return att_alias
-#    
-#    def grab_att_links(self):
-#        return [info['DOWNLOAD_URL'] for info in self.att_res]
-
-
         
 def generic_feature_report(feature_object, output_file):
     import reportlab.platypus as platypus
